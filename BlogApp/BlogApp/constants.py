bf8bbccb6c5b52a7fff8e7583b9a41ecfcf12c2e
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2024-01-22 12:56:36 +0330

    feat(tests): add tests for celery task emailing the blog post user

--- BlogApp/BlogApp/constants.py
+++ BlogApp/BlogApp/constants.py
@@ -1,1 +1,1 @@
-SEND_BLOG_EMAIL_TASK = 'blog.send_blog_email'
+SEND_BLOG_EMAIL_TASK = 'blog.send_blog_email_task'
---
+++ BlogApp/blog/tests/tasks/test_send_blog_email.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+from django.test import TestCase, TransactionTestCase
+from model_mommy import mommy
+
+from blog.tasks import send_blog_email_task
+
+
+class SendBlogEmailTaskTestCase(TestCase):
+    @patch('blog.services.PostUserEmailSender.send_email')
+    def test_send_blog_email(self, email_sender_mock):
+        post = mommy.make('Post')
+        send_blog_email_task(post.id)
+        email_sender_mock.assert_called_once()
